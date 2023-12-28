0a808143d8d6e5e443c557adb4f421fa7b2e170d
grandvegeta <152627265+grandvegeta@users.noreply.github.com>
2023-12-28 15:49:50 +0530

    Delete .config directory

--- .config/dmenu-themes/dark-blue.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-exec dmenu_run -nb "#000000" -nf "#4682B4" -sb "#646464" -sf "#000000" -fn "Ubuntu Mono:bold:size=9"
-
--- .config/networkmanager_dmenu/net.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-exec networkmanager_dmenu -nb '#000000' -nf '#c0a0e4' -sb '#400040' -sf '#ffffff' -fn 'Ubuntu Mono:bold:size=9'
-
